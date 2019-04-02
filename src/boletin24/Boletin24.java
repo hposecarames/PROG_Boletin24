4c5f3025a8a38948da31977a742e88fe842a13dc
menuven <hposecarames.Hector Pose Carames>
2019-04-02 11:57:06 +0200

    Boletin 24

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,18 @@
+
+package boletin24;
+
+/**
+ *
+ * @author Hector Pose Carames
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana obx = new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
